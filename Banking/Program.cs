c93135ff38b38d9fada26f96e6dcad2a8529064d
AaronStPete <AaronStPete@users.noreply.github.com>
2018-04-05 14:52:19 -0400

    asking for user input

--- Banking/Program.cs
+++ Banking/Program.cs
@@ -10,6 +10,37 @@ namespace Banking
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Banking.");
+            Console.WriteLine("Would you like to (login) or create a (new) account?");
+            string input = Console.ReadLine();
+
+            if (input == "login")
+            {
+                /// prompt for name
+                Console.WriteLine("Please login with your name:");
+                Console.ReadLine();
+
+                /// prompt for password
+                Console.WriteLine("Please enter your password");
+                Console.ReadLine();
+
+            }
+            else if (input == "new")
+            {
+                /// generate user
+
+                /// prompt for name
+                Console.WriteLine("");
+                /// store name
+                Console.ReadLine();
+
+
+                /// prompt for password
+                Console.WriteLine("");
+                /// store password
+                Console.ReadLine();
+
+            }
         }
     }
 }
